4e3cb8c61ed762d4723d3af665adb68a4604a76b
Joabe_Alves <joabealvesmorais@alu.ufc.br>
2022-10-25 14:28:16 -0300

    add Node.h file

--- SparseMatrix.h
+++ SparseMatrix.h
@@ -8,22 +8,7 @@
 #ifndef SPARSEMATRIX_H
 #define SPARSEMATRIX_H
 
-/* Estrutura nó da matriz */
-struct Node {
-	Node *right;    // Aponta para o próximo elemento não-nulo da linha
-	Node *down;     // Aponta para o próximo elemento não-nulo da coluna
-	int line;       // Dado da linha
-	int col;        // Dado da coluna
-	double value;   // Valor do elemento
-	/* Construtor que inicializa o nó com seus 5 parâmetros */
-	Node(Node *right, Node *down, int line, int col, double value) {
-		this->right = right;
-		this->down = down;
-		this->line = line;
-		this->col = col;
-		this->value = value;
-	}
-};
+#include "Node.h"
 
 /* TAD da matriz esparsa */
 class SparseMatrix {
