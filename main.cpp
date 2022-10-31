902992f8e1396364fa4c81ce466f96d803c1d53a
Joabe_Alves <joabealvesmorais@alu.ufc.br>
2022-10-31 19:26:20 -0300

    fix insert()

--- main.cpp
+++ main.cpp
@@ -4,23 +4,37 @@
 using namespace std;
 
 int main() {
-	SparseMatrix *matrix = new SparseMatrix(3, 3);
+	SparseMatrix *matrix = new SparseMatrix(9, 9);
 
 	matrix->print();
 	cout << endl;
 
-	matrix->insert(1, 2, 1);
-	matrix->insert(2, 1, 1);
-	matrix->insert(2, 2, 1);
+	for(int i = 4; i <= 6; i++)
+		matrix->insert(1, i, 1);
+
 	matrix->insert(2, 3, 1);
+	matrix->insert(2, 7, 1);
+
 	matrix->insert(3, 2, 1);
+	matrix->insert(3, 8, 1);
+
+	for(int i = 4; i <= 6; i++) {
+		matrix->insert(i, 1, 1);
+		matrix->insert(i, 9, 1);
+	}
+
+	matrix->insert(7, 2, 1);
+	matrix->insert(7, 8, 1);
+
+	matrix->insert(8, 3, 1);
+	matrix->insert(8, 7, 1);
+
+	for(int i = 4; i <= 6; i++)
+		matrix->insert(9, i, 1);
 
 	matrix->print();
 	cout << endl;
 
-	matrix->insert(2, 2, 0);
-
-	matrix->print();
 	cout << endl;
 
 	delete matrix;
